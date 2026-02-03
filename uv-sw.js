a4176d4c3b350ae7984afd78c38917b799f70646
GDG-network <162386566+gdg-network@users.noreply.github.com>
2026-02-03 18:31:35 -0500

    Create uv-sw.js

---
+++ uv-sw.js
@@ -0,0 +1,13 @@
+importScripts("/static/uv/uv.bundle.js");
+importScripts("/static/uv/uv.sw.js");
+
+self.__uv$config = {
+  prefix: "/service/",
+  bare: "https://bare.boomproxy.xyz",
+};
+
+const sw = new UVServiceWorker(self.__uv$config);
+
+self.addEventListener("fetch", event => {
+  event.respondWith(sw.fetch(event));
+});
