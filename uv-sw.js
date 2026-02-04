6229d186d0174f2392617dde13191a974176869e
GDG-network <162386566+gdg-network@users.noreply.github.com>
2026-02-03 19:05:57 -0500

    Update uv-sw.js

--- uv-sw.js
+++ uv-sw.js
@@ -1,13 +1,7 @@
-importScripts("/static/uv/uv.bundle.js");
-importScripts("/static/uv/uv.sw.js");
+importScripts('/static/uv/uv.sw.js');
 
-self.__uv$config = {
-  prefix: "/service/",
-  bare: "https://focus-bare-iota.vercel.app/",
-};
+const sw = new UVServiceWorker();
 
-const sw = new UVServiceWorker(self.__uv$config);
-
-self.addEventListener("fetch", event => {
-  event.respondWith(sw.fetch(event));
-});
+self.addEventListener('fetch', event =>
+  event.respondWith(sw.fetch(event))
+);
